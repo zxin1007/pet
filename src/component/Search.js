c4a3148e9e72332a4456431eed0e325ae535ff53
Zixin Zou <zxinmm@gmail.com>
2022-04-07 12:01:38 -0400

    search field

---
+++ src/component/Search.js
@@ -0,0 +1,36 @@
+import { filter } from "../state/petState";
+import {useRef} from "react"
+import { useDispatch } from "react-redux";
+import styled from 'styled-components'
+
+const Form = styled.form`
+        display: flex;
+        justify-content: center;
+        gap: 1em;
+    `
+
+export default function Search (){
+
+    const searchInput = useRef();
+    const dispatch = useDispatch();
+
+    function handleSumbit(e){
+        e.preventDefault();
+        dispatch(filter(searchInput.current.value))
+    }
+
+    function handleChange(){
+        console.log(searchInput.current.value)
+        dispatch(filter(searchInput.current.value))
+    }
+
+    return (
+        <Form onSubmit={(e)=>handleSumbit(e)}>
+            <input 
+                ref={searchInput}
+                onChange={()=>handleChange()}
+            />
+            <button>Search</button>
+        </Form>
+    )
+}
